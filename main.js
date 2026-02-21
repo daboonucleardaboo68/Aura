a686e92b7fab5acdf51312f794fafcb4a729cc96
daboonucleardaboo68 <daboonucleardaboo@gmail.com>
2026-02-21 13:15:49 +0300

    main.js

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
-    frame: false, // Matches your custom title bar
-    transparent: true,
+    frame: false, 
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     }
   });
-
   win.loadFile('index.html');
 }
 
