fc508b8b889e5bc709c83660b276f2a98f8d5613
daboonucleardaboo68 <daboonucleardaboo@gmail.com>
2026-02-21 13:24:37 +0300

    main.js

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
-    frame: false, 
+    frame: false, // Important for your custom title bar
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     }
   });
+
   win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
 
+// Listeners for your Close and Minimize buttons
 ipcMain.on('close-window', () => app.quit());
-ipcMain.on('minimize-window', () => BrowserWindow.getFocusedWindow()?.minimize());
+ipcMain.on('minimize-window', () => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win) win.minimize();
+});
